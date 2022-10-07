3eece1a79ca21bf2c86be2a735de96e9895c90e7
pdavisDU <pdavisdu@gmail.com>
2022-10-07 11:41:29 -0600

    adding pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Chopz</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+        </Routes>
+      </Router>
     </div>
   );
 }
