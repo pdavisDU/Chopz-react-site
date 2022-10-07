a124b9e550e283c82a6d46de97fb7142c3c4ea3d
pdavisDU <pdavisdu@gmail.com>
2022-10-07 12:30:34 -0600

    added hamburger menu for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/copz-clean.PNG";
 import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/services"> Services </Link>
+          <Link to="/products"> Products </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
@@ -15,6 +29,9 @@ function Navbar() {
         <Link to="/products"> Products </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
