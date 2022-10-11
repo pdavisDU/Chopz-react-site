43418ae433aebf2edc442b158db04f6c4c47be31
pdavisDU <pdavisdu@gmail.com>
2022-10-11 10:50:29 -0600

    added business hours

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,6 +13,9 @@ function Contact() {
         <h1>Location</h1>
         <p>611 Grant Street</p>
         <p>Denver, CO 80203</p>
+        <h1>Hours</h1>
+        <p>Tuesday - Friday 10:30am-7:30pm</p>
+        <p>Saturday 10:30am-5:30pm</p>
       </div>
       <a
         href="https://www.google.com/maps/place/Chopz+Mens+Salon/@39.7261546,-104.9861898,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7ed85f88638b:0x3a22b806e3f35edb!8m2!3d39.7261505!4d-104.984035"
