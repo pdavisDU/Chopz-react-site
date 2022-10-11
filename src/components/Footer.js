902222254db998652cb29660144d711ecd666953
pdavisDU <pdavisdu@gmail.com>
2022-10-11 10:40:31 -0600

    added location icon and link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,24 @@
 import React from "react";
 // importing icons from material UI website
 import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import PlaceIcon from "@mui/icons-material/Place";
 import "../styles/Footer.css";
 function Footer() {
   return (
     <div className="footer">
       {/* creating the DIV with our socials and their icons */}
       <div className="socialMedia">
-        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+        <a href="https://www.instagram.com/chopz_mens_salon/">
+          <InstagramIcon />
+        </a>{" "}
+        <a href="https://www.facebook.com/chopzmensalon">
+          <FacebookIcon />
+        </a>{" "}
+        <a href="https://www.google.com/maps/place/Chopz+Mens+Salon/@39.7261546,-104.9861898,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7ed85f88638b:0x3a22b806e3f35edb!8m2!3d39.7261505!4d-104.984035">
+          {" "}
+          <PlaceIcon />
+        </a>
       </div>
       <p className="address">611 Grant Street</p>
       <p className="address">Denver, CO 80203</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,6 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
         <h1>Chopz Men's Salon</h1>
-        <p> The best haricut ever</p>
         <Link to="/schedule">
           <button> Schedule </button>
         </Link>
